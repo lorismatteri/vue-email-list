c9c4efd2629ab522af8ad36f3f7241dbf9a02e0f
Loris Matteri <loris.matteri93@gmail.com>
2021-01-08 15:39:48 +0100

    Tramite API genero una mail random

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 console.log('ciao');
 
+let app = new Vue({
+    el: "#app",
+    data: {
+    },
+    mounted(){
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then(response => {
+            const randMail = response.data.response
+            console.log(randMail);            
+        });
+            
+    }
+});
